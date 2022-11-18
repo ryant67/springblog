862449b589fba2a02b3d4723b4910a1922dc6591
Ryan Harris <ryan.t.harris67@gmail.com>
2022-11-18 09:24:09 -0600

    PostController code updated

--- src/main/java/com/example/springblog/PostController.java
+++ src/main/java/com/example/springblog/PostController.java
@@ -6,25 +6,25 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class PostController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/posts")
+    @GetMapping("/posts")
     @ResponseBody
     public String posts() {
         return "You are now viewing all Posts!";
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/posts/{id}")
+    @GetMapping("/posts/{id}")
     @ResponseBody
     public String indPosts(@PathVariable int id) {
         return "You are now viewing a Post with the ID of " + id + "!";
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/posts/create")
+    @GetMapping("/posts/create")
     @ResponseBody
     public String createPostForm() {
         return "You are now viewing the form to create a new Post!";
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/posts/create")
+    @PostMapping("/posts/create")
     @ResponseBody
     public String createPost() {
         return "You have now created a new Post!";

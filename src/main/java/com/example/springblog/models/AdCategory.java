50b00a6bf8d4bfd9060e2ade4fbc858c5e0f69f9
Ryan Harris <ryan.t.harris67@gmail.com>
2022-11-27 14:00:29 -0600

    update

--- src/main/java/com/example/springblog/models/AdCategory.java
+++ src/main/java/com/example/springblog/models/AdCategory.java
@@ -1,4 +1,19 @@
 package com.example.springblog.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
 public class AdCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Ad> ads;
 }

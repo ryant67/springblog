4bb253b3b5c36c711dd300c19007c4aaeeb84ec7
Ryan Harris <ryan.t.harris67@gmail.com>
2022-11-27 11:15:50 -0600

    update

--- src/main/java/com/example/springblog/controllers/HomeController.java
+++ src/main/java/com/example/springblog/controllers/HomeController.java
@@ -1,16 +1,58 @@
 package com.example.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    //Establish adsDao instance properties
+    private final AdRepository adsDao;
+    private final OwnerRepository ownerDao;
+    private final EmailService emailService;
+    private final AdService adService;
+    //Anytime this controller is used, inject the adsDao, so we can use it...
+
+    public HomeController(AdRepository adsDao, OwnerRepository ownerDao, EmailService emailService, AdService adservice) {
+        this.adsDao = adsDao;
+        this.ownerDao = ownerDao;
+        this.emailService = emailService;
+        this.adService = adService;
+    }
 
     @GetMapping("/")
     @ResponseBody
     public String landing() {
     return "This is the Landing Page!";
     }
+
+    @GetMapping("/ads")
+    public String allAds(Model model) {
+        List<Ad> ads = adService.getAds();
+        //Other methods to remember;
+        //.save() - insert new record, or update an existing record.
+        //.delete() - delete a record.
+        //.findById() - find record by Id.
+        model.addAttribute("ads", ads);
+        return "ads";
+    }
+
+    @GetMapping("/ads/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("ad", new Ad());
+        return "adCreate";
+    }
+
+    @PostMapping("/ads/create")
+    public String create(@ModelAttribute Ad ad) {
+        adService.createAd(ad);
+        return "redirect:/ads";
+    }
+
+
+
+
 }
--- src/main/java/com/example/springblog/controllers/PostController.java
+++ src/main/java/com/example/springblog/controllers/PostController.java
@@ -6,41 +6,89 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.Binding;
 import java.util.List;
 
 @Controller
 public class PostController {
 
     private PostRepository postDao;
+    private UserRepository userDao;
 
     public PostController(PostRepository postDao) {
         this.postDao = postDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/posts")
     public String posts(Model model) {
+        //Instantiates the list to hold all the Post Objects
         List<Post> allPosts = postDao.findAll();
         model.addAttribute("allPosts", allPosts);
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String indPosts(@PathVariable long id) {
-        return "You are now viewing a Post with the ID of " + id + "!";
+    public String idPosts(@PathVariable long id, Model model) {
+        Post idPosts = postDao.getById(id);
+        model.addAttribute("post", idPosts);
+        return "posts/show";
     }
 
+//    NOT USING FORM MODEL BINDING:
+//    @GetMapping("/posts/create")
+//    public String createPostForm() {
+//        return "posts/create";
+//    }
+
+    //USING FORM MODEL BINDING:
     @GetMapping("/posts/create")
-    public String createPostForm() {
+    public String createPostForm(Model model) {
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
 
+//    NOT USING FORM MODEL BINDING
+//    @PostMapping("/posts/create")
+//    public String createPost(@RequestParam(name="title")String title, @RequestParam(name="description")String description) {
+//        Post post = new Post();
+//        post.setTitle(title);
+//        post.setBody(description);
+//        User user = userDao.getById(1L);
+////        User user = userDao.findById(1L);
+//        post.setUser(user);
+//        postDao.save(post);
+//        return "redirect:/posts";
+//    }
+
+    //USING FORM MODEL BINDING
     @PostMapping("/posts/create")
-    public String createPost(@RequestParam(name="title")String title, @RequestParam(name="description")String description) {
-        Post post = new Post();
-        post.setTitle(title);
-        post.setBody(description);
+    public String createPost(@ModelAttribute Post post) {
+        User user = getById(1L);
+        //User user = userDao.getReferenceById(1L);
+        post.setUser(user);
         postDao.save(post);
         return "redirect:/posts";
     }
+
+    @Getmapping("/posts/{id}/edit")
+    public String editPost(@PathVariable long id, Model model) {
+        model.addAttribute("post", postDao.getReferenceById(id));
+        return "posts/editPost";
+    }
+
+    @PostMapping("/posts/edit")
+    public String editPost(@ModelAttribute Post post) {
+        System.out.println(post.getId());
+        User user = userDao.getById(1L);
+        post.setUser(user);
+        //post.setId(post);
+        post.Dao.save(post);
+        return "redirect:/posts";
+    }
+
+
+
+
+
 }
